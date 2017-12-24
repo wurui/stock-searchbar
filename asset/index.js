f31c0ffce7b919db7f7ece2dc7a6c6b89b697182
wurui <wurui.wr@gmail.com>
2017-12-24 18:19:44 +0800

    first commit

--- asset/index.js
+++ asset/index.js
@@ -1,6 +1,39 @@
-define([],function(){
+define(['oxjs'],function(OXJS){
   return {
     init:function($mod){
+    	var TO,
+            searchInput=$('input',$mod).on('keyup focus',function(){
+                if(TO)clearTimeout(TO);
+                TO=setTimeout(function(){
+                	var val=$.trim(searchInput.val())
+                    $mod.OXRefresh({
+                        'stock-analysis':{
+                            symbol:val
+                        }
+                    });
+                    val?sugguest.show():sugguest.hide();
+                },300)
+            }).on('blur',function(){
+                sugguest.hide()
+            }),
+
+    		sugguest=$('.J_suggest',$mod).css({
+                width:searchInput.width()
+
+            }).hide().on('tap','li',function(){
+				var new_symbol=$.trim(this.innerHTML);
+				searchInput.val(new_symbol).blur();
+				$mod.OXPost({
+					$fav:{
+						'stock-analysis':{
+						    symbol:new_symbol
+						}
+
+					}
+					
+				})
+
+            });
 
     }
   }
--- mod.xsl
+++ mod.xsl
@@ -2,9 +2,20 @@
     <xsl:template match="/root" name="wurui.stock-searchbar">
         <!-- className 'J_OXMod' required  -->
         <div class="J_OXMod oxmod-stock-searchbar" ox-mod="stock-searchbar">
-            <h1>
-                This is mod stock-searchbar;
-            </h1>
+        	<xsl:variable name="list" select="data/stock-analysis/i"/>
+            <div class="searchbar">
+                <div class="J_suggest suggest" style="display:none;">
+                    <ul ox-refresh="html">
+                    	<xsl:if test="count($list) = 0">
+                    		<li class="noresult">no result</li>
+                    	</xsl:if>
+                        <xsl:for-each select="$list">
+                            <li><xsl:value-of select="symbol"/></li>
+                        </xsl:for-each>
+                    </ul>
+                </div>
+                <input type="search" class="J_search" placeholder="input a symbol"/>
+            </div>
         </div>
     </xsl:template>
 </xsl:stylesheet>
